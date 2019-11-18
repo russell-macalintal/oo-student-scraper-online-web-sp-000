3eba011b17ec39711c6b4fdfc33f56590315a532
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-18 03:46:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,36 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    attributes = {}
+    twitter_url = doc.css(".social-icon-container a").detect {|a| a.css("img")[0]["src"] == "../assets/img/twitter-icon.png"}
+    linkedin_url = doc.css(".social-icon-container a").detect {|a| a.css("img")[0]["src"] == "../assets/img/linkedin-icon.png"}
+    github_url = doc.css(".social-icon-container a").detect {|a| a.css("img")[0]["src"] == "../assets/img/github-icon.png"}
+    blog_url = doc.css(".social-icon-container a").detect {|a| a.css("img")[0]["src"] == "../assets/img/rss-icon.png"}
+    profile_quote = doc.css(".profile-quote")
+    bio = doc.css(".bio-content.content-holder .description-holder p")
 
+    if !twitter_url.nil?
+      attributes[:twitter] = twitter_url["href"]
+    end
+    if !linkedin_url.nil?
+      attributes[:linkedin] = linkedin_url["href"]
+    end
+    if !github_url.nil?
+      attributes[:github] = github_url["href"]
+    end
+    if !blog_url.nil?
+      attributes[:blog] = blog_url["href"]
+    end
+    if !profile_quote.nil?
+      attributes[:profile_quote] = profile_quote.text
+    end
+    if !bio.nil?
+      attributes[:bio] = bio.text
+    end
+
+    attributes
   end
 
 end
